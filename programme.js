907ae2fd1561b101919874aedc79aceb55831213
Ahmed hariri <ahmedhariri58@gmail.com>
2024-02-25 16:04:32 +0100

    first commit finich home page

---
+++ programme.js
@@ -0,0 +1,137 @@
+var color_1 = document.getElementById('color-red')
+var color_2 = document.getElementById('color-yellow')
+var color_3 = document.getElementById('color-blue')
+var color_4 = document.getElementById('color-black')
+
+
+color_1.onclick = function () {
+    document.querySelector("img").src = './assets/red-nike.png'
+    document.querySelector("img").style.width = '60vw'
+    document.querySelector("img").style.height = '55vh'
+}
+color_2.onclick = function () {
+    document.querySelector("img").src = './assets/yellow-nike.png'
+    document.querySelector("img").style.width = '60vw'
+    document.querySelector("img").style.height = '55vh'
+}
+color_3.onclick = function () {
+    document.querySelector("img").src = './assets/blue-nike.png'
+    document.querySelector("img").style.width = '60vw'
+    document.querySelector("img").style.height = '55vh'
+}
+color_4.onclick = function () {
+    document.querySelector("img").src = './assets/black-nike.png'
+    document.querySelector("img").style.width = '60vw'
+    document.querySelector("img").style.height = '55vh'
+}
+
+var img_1 = document.getElementById("test-1")
+var img_2 = document.getElementById("test-2")
+var img_3 = document.getElementById("test-3")
+var img_4 = document.getElementById("test-4")
+
+img_1.onclick = function () {
+
+    document.querySelector("img").src = './assets/default-nike.png'
+    document.querySelector("img").style.width = '60vw'
+    document.querySelector("img").style.height = '55vh'
+
+    color_1.onclick = function () {
+        document.querySelector("img").src = './assets/red-nike.png'
+        document.querySelector("img").style.width = '60vw'
+        document.querySelector("img").style.height = '55vh'
+    }
+    color_2.onclick = function () {
+        document.querySelector("img").src = './assets/yellow-nike.png'
+        document.querySelector("img").style.width = '60vw'
+        document.querySelector("img").style.height = '55vh'
+    }
+    color_3.onclick = function () {
+        document.querySelector("img").src = './assets/blue-nike.png'
+        document.querySelector("img").style.width = '60vw'
+        document.querySelector("img").style.height = '55vh'
+    }
+    color_4.onclick = function () {
+        document.querySelector("img").src = './assets/black-nike.png'
+        document.querySelector("img").style.width = '60vw'
+        document.querySelector("img").style.height = '55vh'
+    }
+
+}
+
+img_2.onclick = function () {
+    document.querySelector("img").src = './assets/white-tn.png'
+    document.querySelector("img").style.width = '60vw'
+    document.querySelector("img").style.height = '55vh'
+    color_1.onclick = function () {
+        document.querySelector("img").src = './assets/red-tn.png'
+        document.querySelector("img").style.width = '40vw'
+        document.querySelector("img").style.height = '45vh'
+    }
+    color_2.onclick = function () {
+        document.querySelector("img").src = './assets/yellow-tn.png'
+        document.querySelector("img").style.width = '70vw'
+        document.querySelector("img").style.height = '60vh'
+    }
+    color_3.onclick = function () {
+        document.querySelector("img").src = './assets/blue-tn.png'
+        document.querySelector("img").style.width = '70vw'
+        document.querySelector("img").style.height = '60vh'
+    }
+    color_4.onclick = function () {
+        document.querySelector("img").src = './assets/black-tn.png'
+        document.querySelector("img").style.width = '65vw'
+        document.querySelector("img").style.height = '55vh'
+    }
+
+}
+img_3.onclick = function () {
+    document.querySelector("img").src = './assets/default-3.png'
+    document.querySelector("img").style.width = '60vw'
+    document.querySelector("img").style.height = '55vh'
+    color_1.onclick = function () {
+        document.querySelector("img").src = './assets/sebadri-red.png'
+        document.querySelector("img").style.width = '40vw'
+        document.querySelector("img").style.height = '45vh'
+    }
+    color_2.onclick = function () {
+        document.querySelector("img").src = './assets/sebadri-yellow.png'
+        document.querySelector("img").style.width = '70vw'
+        document.querySelector("img").style.height = '60vh'
+    }
+    color_3.onclick = function () {
+        document.querySelector("img").src = './assets/sebadri-blue.png'
+        document.querySelector("img").style.width = '60vw'
+        document.querySelector("img").style.height = '55vh'
+    }
+    color_4.onclick = function () {
+        document.querySelector("img").src = './assets/sebadri-black.png'
+        document.querySelector("img").style.width = '65vw'
+        document.querySelector("img").style.height = '55vh'
+    }
+}
+img_4.onclick = function () {
+    document.querySelector("img").src = './assets/default-4.png'
+    document.querySelector("img").style.width = '60vw'
+    document.querySelector("img").style.height = '55vh'
+    color_1.onclick = function () {
+        document.querySelector("img").src = './assets/sebadri-2-red.png'
+        document.querySelector("img").style.width = '40vw'
+        document.querySelector("img").style.height = '45vh'
+    }
+    color_2.onclick = function () {
+        document.querySelector("img").src = './assets/sebadri-2-yellow.png'
+        document.querySelector("img").style.width = '70vw'
+        document.querySelector("img").style.height = '60vh'
+    }
+    color_3.onclick = function () {
+        document.querySelector("img").src = './assets/sebadri-2-blue.png'
+        document.querySelector("img").style.width = '70vw'
+        document.querySelector("img").style.height = '60vh'
+    }
+    color_4.onclick = function () {
+        document.querySelector("img").src = './assets/sebadri-2-black.png'
+        document.querySelector("img").style.width = '65vw'
+        document.querySelector("img").style.height = '55vh'
+    }
+}
